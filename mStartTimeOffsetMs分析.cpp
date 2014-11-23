cae8bde95febc3e5cd77719036a15d40c933bd6b
liang.wang <liang.wang@ck-telecom.com>
2014-11-23 17:56:38 +0800

    analysis first smaple timple processing

--- mStartTimeOffsetMs分析.cpp
+++ mStartTimeOffsetMs分析.cpp
@@ -1,22 +1,27 @@
     ALOGV("[%s], [%d], mStartTimeOffsetMs:%d",__FUNCTION__,__LINE__, mStartTimeOffsetMs);
 
-	R:\mt6595_kk\out\target\product\ckt95_lw_kk\system\lib>adb logcat -v time -s StagefrightRecorder MPEG4Writer  | grep -E "mStartTimeOffsetMs|startTimeOffsetUs|Start time offset|startTimeUs|mStartTimeRealUs"
-01-06 02:53:39.941 D/StagefrightRecorder( 4350): [setupMPEG4Recording], [1804], mStartTimeOffsetMs:-1
-01-06 02:53:39.941 D/StagefrightRecorder( 4350): [startMPEG4Recording] startTimeUs=2416676724
-01-06 02:53:39.943 D/MPEG4Writer( 4350): video Track  start,mStartTimeRealUs=2416676724
-01-06 02:53:39.943 D/MPEG4Writer( 4350): [start], [2843], startTimeOffsetUs:-1000
-01-06 02:53:39.943 D/MPEG4Writer( 4350): [start], [2848], startTimeOffsetUs:700000
-01-06 02:53:39.943 D/MPEG4Writer( 4350): [start], [2857], startTimeUs:2416676724
-01-06 02:53:39.943 I/MPEG4Writer( 4350): Start time offset: 700000 us
-01-06 02:53:39.943 D/MPEG4Writer( 4350): [start], [2861], startTimeUs:2417376724
-01-06 02:53:40.530 D/MPEG4Writer( 4350): audio Track  start,mStartTimeRealUs=2416676724
-01-06 02:53:40.530 D/MPEG4Writer( 4350): [start], [2843], startTimeOffsetUs:-1000
-01-06 02:53:40.530 D/MPEG4Writer( 4350): [start], [2848], startTimeOffsetUs:700000
-01-06 02:53:40.530 D/MPEG4Writer( 4350): [start], [2857], startTimeUs:2416676724
-01-06 02:53:40.530 I/MPEG4Writer( 4350): Start time offset: 700000 us
-01-06 02:53:40.530 D/MPEG4Writer( 4350): [start], [2861], startTimeUs:2417376724    //最终的startTimeUs就是在原来的基础上加了70000
+	开始时间的处理会考虑到音视频同步，丢音频帧。needDropAudioFrame这里先不研究。
 
-=================================================================StagefrightRecorder=========================================================================
+	R:\mt6595_kk\out\target\product\ckt95_lw_kk\system\lib>adb logcat -v time -s StagefrightRecorder MPEG4Writer  | grep -E "mStartTimeOffsetMs|startTimeOffsetUs|Start time offset|startTimeUs|mStartTimeRealUs|mStartTimestampUs"
+	01-06 03:32:35.100 D/StagefrightRecorder( 4350): [setupMPEG4Recording], [1804], mStartTimeOffsetMs:-1
+	01-06 03:32:35.100 D/StagefrightRecorder( 4350): [startMPEG4Recording] startTimeUs=4751835177
+	01-06 03:32:35.102 D/MPEG4Writer( 4350): video Track  start,mStartTimeRealUs=4751835177
+	01-06 03:32:35.102 D/MPEG4Writer( 4350): [start], [2843], startTimeOffsetUs:-1000
+	01-06 03:32:35.102 D/MPEG4Writer( 4350): [start], [2848], startTimeOffsetUs:700000
+	01-06 03:32:35.102 D/MPEG4Writer( 4350): [start], [2857], startTimeUs:4751835177
+	01-06 03:32:35.102 I/MPEG4Writer( 4350): Start time offset: 700000 us
+	01-06 03:32:35.102 D/MPEG4Writer( 4350): [start], [2861], startTimeUs:4752535177
+	01-06 03:32:35.621 D/MPEG4Writer( 4350): audio Track  start,mStartTimeRealUs=4751835177
+	01-06 03:32:35.621 D/MPEG4Writer( 4350): [start], [2843], startTimeOffsetUs:-1000
+	01-06 03:32:35.621 D/MPEG4Writer( 4350): [start], [2848], startTimeOffsetUs:700000
+	01-06 03:32:35.621 D/MPEG4Writer( 4350): [start], [2857], startTimeUs:4751835177
+	01-06 03:32:35.621 I/MPEG4Writer( 4350): Start time offset: 700000 us
+	01-06 03:32:35.621 D/MPEG4Writer( 4350): [start], [2861], startTimeUs:4752535177    //最终的startTimeUs就是在原来的基础上加了70000
+	01-06 03:32:36.033 D/MPEG4Writer( 4350): Video mStartTimestampUs=16892us
+	01-06 03:32:36.114 D/MPEG4Writer( 4350): Audio mStartTimestampUs=105915us
+	01-06 03:32:37.042 D/MPEG4Writer( 4350): Audio mStartTimestampUs = 105915, moovStartTimeUs16892: //反复测试, moovStartTimeUs就是mStartTimestampUs，Video mStartTimestampUs
+
+########################################################StagefrightRecorder########################################################
 StagefrightRecorder，MPEG4Writer都定义了mStartTimeOffsetMs，目前看来StagefrightRecorder的没有作用。
 
 mStartTimeOffsetMs: 是-1，没有用
@@ -43,7 +48,7 @@ kKeyTime:
 		01-06 02:45:44.648 D/MPEG4Writer( 2662): audio Track  start,mStartTimeRealUs=1940908437
 
 
-=================================================================MPEG4Writer=========================================================================
+########################################################MPEG4Writer########################################################
 MPEG4Writer的mStartTimeOffsetMs没有直接访问，都是通过函数取得
 ./include/media/stagefright/MPEG4Writer.h:87:    int32_t getStartTimeOffsetMs() const { return mStartTimeOffsetMs; }
 
@@ -54,3 +59,24 @@ startTimeUs:
 	最终的时间会用到:
 		meta->setInt64(kKeyTime, startTimeUs);
 	status_t err = mSource->start(meta.get()); 
+
+*********************************研究threadEntry中处理时间
+mFirstSampleTimeRealUs：做调试用
+        int64_t initialDelayUs =
+            mFirstSampleTimeRealUs - mStartTimeRealUs - startTimeOffsetUs;
+
+下面这段代码
+		if (mStszTableEntries->count() == 0) {
+            mFirstSampleTimeRealUs = systemTime() / 1000;
+            mStartTimestampUs = timestampUs; //
+            mOwner->setStartTimestampUs(mStartTimestampUs); //mStartTimestampUs是timestampUs
+            previousPausedDurationUs = mStartTimestampUs;
+#ifndef ANDROID_DEFAULT_CODE
+			ALOGD("%s mStartTimestampUs=%lldus", mIsAudio?"Audio":"Video", mStartTimestampUs);
+			if (!mIsAudio) {
+				mOwner->setVideoStartTimeUs(mStartTimestampUs); //mVideoStartTimeUs是timestampUs
+				mOwner->notify(MEDIA_RECORDER_EVENT_INFO, MEDIA_RECORDER_INFO_START_TIMER, 0); //以收到video第一个sample作为录像的开始
+			}
+#endif
+        }
+	
